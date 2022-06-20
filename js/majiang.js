bea4bfec5393d5f47834e52296e2a1ffa27caccd
BuildTools <1773701277@qq.com>
2022-06-20 19:47:53 +0800

    first commit

---
+++ js/majiang.js
@@ -0,0 +1,9 @@
+/*
+ *  Majiang
+ *
+ *  Copyright(C) 2015−2017 Satoshi Kobayashi
+ *  Released under the MIT license
+ *  http://kobalab.net/majiang/LICENSE
+ */
+
+var Majiang = { VERSION: '0.8.7' };
